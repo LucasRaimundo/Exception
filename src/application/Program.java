d9550ba5ac033aa48613c850b5b474ffa558a397
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-05-24 13:48:20 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,44 +10,38 @@ import model.entities.Reservation;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner ler = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number: ");
 		int number = ler.nextInt();
 		System.out.print("Check in: date(dd/MM/yyyy) ");
 		Date checkIn = sdf.parse(ler.next());
 		System.out.print("Check out: date(dd/MM/yyyy) ");
 		Date checkOut = sdf.parse(ler.next());
-		
+
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check out date must be after check in date");
 		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-		
-		
-		System.out.println();
-		System.out.println("Enter data update the reservation:");
-		System.out.print("Check in: date(dd/MM/yyyy) ");
-		checkIn = sdf.parse(ler.next());
-		System.out.print("Check out: date(dd/MM/yyyy) ");
-		checkOut = sdf.parse(ler.next());
-		
-		Date now = new Date();
-		if (checkIn.before(now) || (checkOut.before(now))){
-			System.out.println("Error in reservation: Reservation dates for update must be future date");
-		} else if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check out date must be after check in date");
-		} else {
-		
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
+			System.out.println();
+			System.out.println("Enter data update the reservation:");
+			System.out.print("Check in: date(dd/MM/yyyy) ");
+			checkIn = sdf.parse(ler.next());
+			System.out.print("Check out: date(dd/MM/yyyy) ");
+			checkOut = sdf.parse(ler.next());
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+
+			} else {
+				System.out.println("Reservation: " + reservation);
 			}
 		}
-		
-		
+
 		ler.close();
 
 	}
